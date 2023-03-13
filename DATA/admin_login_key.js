2f98b53d3d79fd5e7fa351b1ab5cb5d13d066283
Sahitya Neupane <neupanesahitya1@gmail.com>
2023-03-13 06:34:17 +0545

    pms

---
+++ DATA/admin_login_key.js
@@ -0,0 +1,17 @@
+const key = [
+  {
+    userName: "neupanesahitya1@gmail.com",
+    firstName: "sahitya",
+    lastName: "neupaene",
+    password: "Sahitya*",
+  },
+  {
+    userName: "shresthagaurav1@gmail.com",
+    firstName: "gaurav",
+    lastName: "shrestha",
+    password: "gauravhero.*",
+  },
+];
+export default key;
+// export { key };
+// module.exports = { key };
---
+++ key.js
@@ -0,0 +1,18 @@
+const key = [
+  {
+    userName: "neupanesahitya1@gmail.com",
+    firstName: "sahitya",
+    lastName: "neupaene",
+    password: "SahiIya1021*",
+  },
+  {
+    userName: "shresthagaurav1@gmail.com",
+    firstName: "gaurav",
+    lastName: "shrestha",
+    password: "Gauravhero.*",
+  },
+];
+
+export default key;
+// export { key };
+// module.exports = { key };
